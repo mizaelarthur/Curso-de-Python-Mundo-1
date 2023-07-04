8c5a4eca2bd8b9b0264df14d439e527ae67d8c65
mizaelarhtur <mizaelarthur2001@gmail.com>
2023-07-04 20:09:01 -0300

    commit

---
+++ Exercicios/exercicio009.py
@@ -0,0 +1,4 @@
+#Faça um programa que leia um número inteiro qualquer e mostre na tela sua tabuada
+
+n=int(input('Digite o número: '))
+print('A tabuada para o número informado é: \n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}'.format(n*1,n*2,n*3,n*4,n*5,n*6,n*7,n*8,n*9,n*10,))
