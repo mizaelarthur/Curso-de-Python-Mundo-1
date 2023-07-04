21d0b7f4145104c644d4e9495d491f9d72e90afa
M1Z43L <mizaelarthur2001@gmail.com>
2023-07-04 10:00:01 -0300

    Exercicio 005

---
+++ exercicio005.py
@@ -0,0 +1,8 @@
+# Faça um programa que leia um número inteiro e mostre na tela:
+# Seu antecessor e seu sucessor
+
+n=int(input('Digite o número: '))
+a=n-1
+s=n+1
+
+print('O antecessor e sucessor do seu número são, respecitivamente:\n {} e {}'.format(a,s))
