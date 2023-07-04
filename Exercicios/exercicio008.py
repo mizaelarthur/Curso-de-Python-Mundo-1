9fbdb65274c77ac9c7c1528c8ed07bd61412635c
mizaelarhtur <mizaelarthur2001@gmail.com>
2023-07-04 10:49:44 -0300

    Exercicio 008

---
+++ Exercicios/exercicio008.py
@@ -0,0 +1,6 @@
+# Faça um programa que leia a distancia em metros e exiba em:
+# Centímetros e Milímetros
+
+n=float(input('Digite a distancia em metros: '))
+print('A distancia em centímetros e milímetros é, especitivamente: \n {:.2f} e {:.2f}'.format(n*100,n*1000))
+
