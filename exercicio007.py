91b7e0c57e0fbc64a7d0762c0436def20181db72
M1Z43L <mizaelarthur2001@gmail.com>
2023-07-04 10:13:26 -0300

    Exercicios

---
+++ exercicio007.py
@@ -0,0 +1,6 @@
+# Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
+
+n1=(int(input('Informe a primeira nota do aluno: ')))
+n2=(int(input('Informe a segunda nota do aluno: ')))
+
+print('A média das notas é: {}'.format((n1+n2)/2))
